9ff31797de08dbf156f3d55aeabdc3265866672f
hyeonmi3 <cc146@daum.net>
2023-03-07 19:55:11 +0900

    add 추가

---
+++ src/component/add.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+import '../style/add.css';
+
+import Aside from './aside.js';
+import Background from './background.js';
+
+const Add = () => {
+    return (
+        <div className='Add'>
+            <div>
+                
+            </div>
+            <Aside />
+            <Background />
+        </div>
+    );
+}
+
+export default Add;
