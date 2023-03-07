733ca90e8b55a6a79f4af7ec3e4b3a4a59044438
hyeonmi3 <cc146@daum.net>
2023-03-07 19:55:33 +0900

    background

---
+++ src/component/background.js
@@ -0,0 +1,21 @@
+import '../style/background.css';
+
+import {Link} from 'react-router-dom';
+
+const Background = () => {
+    return (
+        <div className="Background">
+            <>
+                <div className='board'>
+                    <ul className='board_button'>
+                        <Link to='/' className='board_home_btn'><li><h4>Home</h4></li></Link>
+                        <Link to='/add' className='board_add_btn'><li><h4>Add</h4></li></Link>
+                    </ul>
+                </div>
+                <div className='bg_color'></div>
+            </>
+        </div>
+    );
+}
+
+export default Background;
